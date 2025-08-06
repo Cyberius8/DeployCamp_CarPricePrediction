d7652a1368620ea23b64318b8012fd18e980f93c
raihanizar <raihannizar24@gmail.com>
2025-08-06 02:01:35 +0000

    feat: integrasi model dari dagshub

--- api/router/prediction.py
+++ api/router/prediction.py
@@ -1,13 +1,20 @@
 from fastapi import APIRouter
 from pydantic import ValidationError
-from services.ml_service import MLService, CarPredictionRequest, CarPredictionResponse
+from services.ml_service import MLService, CarPredictionRequestTest, CarPredictionRequestRFQ, CarPredictionResponse
 
 router = APIRouter()
 ml_service = MLService()
 
 # convert json body request to dict
-@router.post("/predict", response_model=CarPredictionResponse)
-async def predict(request: CarPredictionRequest):
+@router.post("/predict-test")
+async def predict_test(request: CarPredictionRequestTest):
   data = request.dict()
-  result = ml_service.predict(data)
+  result = ml_service.predict(data, "test")
+  return result
+
+# convert json body request to dict
+@router.post("/predict-rfq")
+async def predict_rfq(request: CarPredictionRequestRFQ):
+  data = request.dict()
+  result = ml_service.predict(data, "rfq")
   return result
--- api/services/ml_service.py
+++ api/services/ml_service.py
@@ -1,11 +1,12 @@
 import pickle
 import pandas as pd
+import mlflow
 from pathlib import Path
 from pydantic import BaseModel
 from datetime import datetime
 
 # Request model
-class CarPredictionRequest(BaseModel):
+class CarPredictionRequestTest(BaseModel):
     fueltype: int
     aspiration: int
     doornumber: int
@@ -30,6 +31,18 @@ class CarPredictionRequest(BaseModel):
     highwaympg: float
     brand: int
 
+# Request model for Random Forest Quantile Regression
+class CarPredictionRequestRFQ(BaseModel):
+    brand: int
+    wheelbase: float
+    carlength: float
+    carwidth: float
+    curbweight: float
+    enginesize: float
+    fuelsystem: int
+    boreratio: float
+    horsepower: float
+
 # Response model  
 class CarPredictionResponse(BaseModel):
     prediction: dict
@@ -38,28 +51,35 @@ class CarPredictionResponse(BaseModel):
 
 class MLService:
   def __init__(self):
-    # model_path = Path(__file__)
-    # with open(model_path, "rb") as f:
-      # self.model = pickle.load(f)
-    pass
+    mlflow.set_tracking_uri("https://dagshub.com/Cyberius8/DeployCamp_CarPricePrediction.mlflow")
+    ml_flow_client = mlflow.MlflowClient()  
     
-  def predict(self, data):
+    self.model = mlflow.pyfunc.load_model(f"models:/QuantileRF/2")
+    
+  def predict(self, data, model_type):
     X = pd.DataFrame([data])
-    # prediction = self.model.predict(X)[0]
-    # proba = self.model.predict_proba(X)[0]
-    exact_value = 500000000
-    range_min = exact_value - 50000000
-    range_max = exact_value + 50000000
-    confidence = 0.9
-        
+
+    if model_type == "rfq":
+      predictions = self.model.predict(X).tolist()
+      range_min = predictions[0][0]
+      exact_value = predictions[1][0]
+      range_max = predictions[2][0]
+      confidence = 0.95
+
+    if model_type == "test": 
+      range_min = exact_value - 50000000
+      exact_value = 500000000
+      range_max = exact_value + 50000000
+      confidence = 0
+
     return {
       "prediction": {
         "price_exact": int(exact_value),
         "price_range": {
-          "min": range_min,
-          "max": range_max
+          "min": int(range_min),
+          "max": int(range_max)
         },
-        "confidence": round(confidence, 2)
+        "confidence": confidence
       },
       "currency": "IDR",
       "timestamp": datetime.now().isoformat()
